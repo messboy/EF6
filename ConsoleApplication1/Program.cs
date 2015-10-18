b06e0d1cb3dd00e80b136f545cc6f7434b1c9bb9
messboy <messboy000@gmail.com>
2015-10-18 12:00:33 +0800

    加入Day1 練習

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -75,7 +75,76 @@ namespace ConsoleApplication1
             }
         }
 
+        private static void Day1_Practics(ContosoUniversityEntities db)
+        {
+            // db.Configuration.ProxyCreationEnabled = false;
 
+            var data = from p in db.Department
+                       select p;
+
+            foreach (var department in data)
+            {
+                Console.WriteLine(department.DepartmentID + "\t" + department.Name);
+
+                //var courses = from q in db.Course
+                //              where q.DepartmentID == department.DepartmentID
+                //              select q;
+
+                //var courses = department.Course;
+
+                foreach (var course in department.Course)
+                {
+                    Console.WriteLine("\t" + course.CourseID + "\t" + course.Title);
+                }
+            }
+
+            Console.WriteLine("--------------------------------------------------------");
+
+            foreach (var c in db.Course)
+            {
+                Console.WriteLine(c.CourseID + "\t" + c.Title + "\t" + c.Department.Name);
+            }
+
+            Console.WriteLine("--------------------------------------------------------");
+
+            //var c1 = new Course()
+            //{
+            //    Title = "Test",
+            //    Credits = 5,
+            //    DepartmentID=4
+            //};
+
+            //db.Course.Add(c1);
+            //db.SaveChanges();
+
+            //foreach (var c in db.Course.AsNoTracking())
+            //{
+            //    Console.WriteLine(c.CourseID + "\t" + c.Title + "\t" + c.Department.Name);
+            //}
+
+
+            //Console.WriteLine("--------------------------------------------------------");
+
+            var c2 = db.Course.Find(7);
+            c2.Instructors.Add(db.Person.Find(5));
+            db.SaveChanges();
+
+
+            // 取得 SQL Server 伺服器時間的方法
+            DateTime dt = db.Database.SqlQuery<DateTime>("select getdate()").First();
+
+
+            var sql = @"SELECT Course.CourseID, Course.Title, Course.Credits, Department.Name AS DepartmentName FROM Course INNER JOIN Department ON Course.DepartmentID = Department.DepartmentID";
+
+            var data2 = db.Database.SqlQuery<Course>(sql);
+
+            foreach (var item in data2)
+            {
+                Console.WriteLine(item.CourseID + "\t" + item.Title + "\t");
+            }
+
+            Console.WriteLine("--------------------------------------------------------");
+        }
 
     }
 }
